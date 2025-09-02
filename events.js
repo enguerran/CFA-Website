f155edc8e7e4ce82d5e07f10eb950024f3d8f7fa
unkn0wnd.fbx <titouan.helbert@gmail.com>
2025-09-02 20:28:58 +0200

    fete village add

--- events.js
+++ events.js
@@ -1,15 +1,15 @@
-document.querySelectorAll(".addToCalendar").forEach(button => {
-    button.addEventListener("click", function () {
-        const title = this.getAttribute("data-title");
-        const description = this.getAttribute("data-description");
-        const location = this.getAttribute("data-location");
-        const start = this.getAttribute("data-start");
-        const end = this.getAttribute("data-end");
+document.querySelectorAll('.addToCalendar').forEach(button => {
+  button.addEventListener('click', function () {
+    const title = this.getAttribute('data-title')
+    const description = this.getAttribute('data-description')
+    const location = this.getAttribute('data-location')
+    const start = this.getAttribute('data-start')
+    const end = this.getAttribute('data-end')
 
-        const cal = ics();
+    const cal = ics()
 
-        cal.addEvent(title, description, location, start, end);
-        const fileName = title.replace(/\s+/g, "_") + ".ics";
-        cal.download(fileName);
-    });
-});
+    cal.addEvent(title, description, location, start, end)
+    const fileName = title.replace(/\s+/g, '_') + '.ics'
+    cal.download(fileName)
+  })
+})
